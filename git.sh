9ad45159fcc4b85d7833ebad21b5efe33e17e96d
Lulliter <lmm76@georgetown.edu>
2024-06-18 16:35:11 +0200

    upd portfolio R & link in data/projects*.csv

--- git.sh
+++ git.sh
@@ -20,7 +20,8 @@ git add "03_CV_vitae/resume_europrogettista.pdf"
   git add LMMimmi_RES_ENG_sanitized.pdf
   git add LMMimmi_RES_ITA_sanitized.Rmd
   git add README.md README.html
-
+  git add LMMimmi_RES_ISPI_ITA.Rmd
+  git add       LMMimmi_RES_ISPI_ITA.pdf
         
 git add awesome_RES_ENG.Rmd
 git add  awesome_RES_ENG.pdf
@@ -30,7 +31,7 @@ git add  awesome_longCV_ENG.pdf
 # commit changes 
 git commit -m "1 hacked kable extra lines + 2 footer is back" -m "1  Had some issues with the header-includes: LaTeX packages, see https://github.com/haozhu233/kableExtra/issues/413 2 reinstalled vitae from Github"
 git commit -m "added 1 job to Resume ITA in LongCVs" -m "both data input (.csv) and .Rmd files"
-git commit -m "added LMMimmi_RES_FORM_ITA.Rmd X AVERE VERSIONE FORMATORE | no param"  
+git commit -m "added LMMimmi_RES_ISPI_ITA.Rmd"  
 git commit -m "modified LMMimmi_RES_ENG.Rmd"  -m "and added to website as CV "
 
 # push 
